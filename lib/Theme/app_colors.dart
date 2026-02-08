eb3815c9de6b5eb73d7d6108642b6bcc2d4119a4
Utsav <pandyautsav3108@gmail.com>
2026-02-08 23:01:59 +0530

    Fixed category selection issue of lower case.

--- lib/Theme/app_colors.dart
+++ lib/Theme/app_colors.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  AppColors._();
+  AppColors._(); // Private constructor to prevent instantiation
 
   // Background
   static const Color background = Colors.black;
 
-  // Sticky text
+  // Text colors
   static const Color textPrimary = Colors.white;
   static const Color textHint = Colors.white54;
 
-  // Chips
-  static const Color textChip = Color(0xFFD8DD56);
-  static const Color photoChip = Color(0xFF7C74F5);
-  static const Color videoChip = Color(0xFFF5AA74);
+  // Sticky note type colors (Chips)
+  static const Color textChip = Color(0xFFD8DD56);    // Yellow
+  static const Color photoChip = Color(0xFF7C74F5);   // Purple
+  static const Color videoChip = Color(0xFFF5AA74);   // Orange
 
-  // Save button
-  static const Color saveButton = Color(0xFFA4F291);
+  // Button colors
+  static const Color saveButton = Color(0xFFA4F291);  // Green
 
-  // Opacity
+  // Opacity levels
   static const double activeOpacity = 1.0;
   static const double inactiveOpacity = 0.5;
 }
